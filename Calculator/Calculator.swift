8109732c576e6549ca19283caaeee6f93348d5b0
Julius <juliuskempen13@gmail.com>
2021-12-02 17:01:52 +0100

    test

---
+++ Calculator/Calculator.swift
@@ -0,0 +1,144 @@
+//
+//  Calculator.swift
+//  Calculator
+//
+//  Created by Julius Kempen on 01/12/2021.
+//
+
+import Foundation
+
+struct button {
+    let id = UUID()
+    var name: String
+    var sign: String
+}
+
+struct Calculator {
+    var value = "0"
+    var previousvalue = "0"
+    var action = ""
+    var calculation = 0.0
+    var equal_is_pushed = false
+    var decimal_is_used = false
+    var D = 1
+    
+    mutating func buttonpress(button: button){
+        switch button.sign {
+        case one.sign, two.sign, three.sign, four.sign, five.sign, six.sign, seven.sign, eight.sign, nine.sign, zero.sign:
+            if equal_is_pushed == true {
+                previousvalue = value
+                decimal_is_used = false
+            }
+            
+            if value == "0" || equal_is_pushed == true {
+                value = button.sign
+                equal_is_pushed = false
+            }
+            else {
+                value = "\(value)\(button.sign)"
+            }
+            
+        case plus.sign:
+            if equal_is_pushed == true {
+                equal_is_pushed = false
+            }
+            action = "+"
+            calculation = Double(value) ?? 0
+            value = "0"
+            decimal_is_used = false
+            
+        case minus.sign:
+            if equal_is_pushed == true {
+                equal_is_pushed = false
+            }
+            action = "-"
+            calculation = Double(value) ?? 0
+            value = "0"
+            decimal_is_used = false
+            
+        case times.sign:
+            if equal_is_pushed == true {
+                equal_is_pushed = false
+            }
+            action = "x"
+            calculation = Double(value) ?? 0
+            value = "0"
+            decimal_is_used = false
+            
+        case devide.sign:
+            if equal_is_pushed == true {
+                equal_is_pushed = false
+            }
+            action = "÷"
+            calculation = Double(self.value) ?? 0
+            value = "0"
+            decimal_is_used = false
+            
+        case dot.sign:
+            if value.suffix(1) != "." && decimal_is_used != true && equal_is_pushed != true {
+                decimal_is_used = true
+                value = "\(value)\(dot.sign)"
+            }
+            
+        case plusminus.sign:
+            value = "\(-(Double(value) ?? 0))"
+        
+        case percentage.sign:
+            value = "\((Double(value) ?? 0) * 0.01)"
+            decimal_is_used = true
+            
+        case equal.sign:
+            if action == "+"{
+                value = "\((Double(value) ?? 0) + calculation)"
+            }
+            else if action == "-"{
+                value = "\((Double(value) ?? 0) - calculation)"
+            }
+            else if action == "x"{
+                value = "\((Double(value) ?? 0) * calculation)"
+            }
+            else if action == "÷"{
+                value = "\((Double(calculation) / (Double(value) ?? 0)))"
+            }
+            action = ""
+            equal_is_pushed = true
+        
+        case clear.sign:
+            value = "0"
+            calculation = 0
+            action = ""
+            equal_is_pushed = false
+            decimal_is_used = false
+            
+        default:
+            D = 1
+            
+        }
+    }
+}
+    
+
+let zero = button(name: "zero", sign: "0")
+let one = button(name: "one", sign: "1")
+let two = button(name: "two", sign: "2")
+let three = button(name: "three", sign: "3")
+let four = button(name: "four", sign: "4")
+let five = button(name: "five", sign: "5")
+let six = button(name: "six", sign: "6")
+let seven = button(name: "seven", sign: "7")
+let eight = button(name: "eight", sign: "8")
+let nine = button(name: "nine", sign: "9")
+let equal = button(name: "equal", sign: "=")
+let dot = button(name: "dot", sign: ".")
+let plus = button(name: "plus", sign: "+")
+let minus = button(name: "minus", sign: "-")
+let times = button(name: "times", sign: "x")
+let devide = button(name: "devide", sign: "÷")
+let percentage = button(name: "percentage", sign: "%")
+let plusminus = button(name: "plusminus", sign: "+/-")
+let clear = button(name: "clear", sign: "AC")
+
+
+
+
+
--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -7,15 +7,98 @@
 
 import SwiftUI
 
+extension Color {
+    static let customcolor = Color("calculatorbackround")
+}
+
 struct ContentView: View {
+    
+    @State var calculator: Calculator = Calculator()
+    
+    let buttons: [[button]] = [
+        [clear, plusminus, percentage, devide],
+        [seven, eight, nine, times],
+        [four, five, six, minus],
+        [one, two, three, plus],
+        [zero, dot, equal]
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color.customcolor
+                .edgesIgnoringSafeArea(.all)
+                
+            VStack {
+                VStack {
+                    Capsule()
+                        .foregroundColor(Color(hue: 0.573, saturation: 0.925, brightness: 0.988))
+                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 0)
+                        .frame(height: 60)
+                        .overlay(Text("Kempen Instruments")
+                                    .font(.system(size: 30))
+                                    .fontWeight(.semibold)
+                                    .foregroundColor(Color.white)
+                        )
+                        .padding()
+                    Spacer()
+                    
+                    VStack {
+                        HStack{
+                            Spacer()
+                            Text(calculator.previousvalue.prefix(9))
+                                .foregroundColor(.white)
+                                .bold()
+                                .font(.system(size: 60))
+                                .padding()
+                        }
+                        HStack{
+                            Spacer()
+                            Text(calculator.value.prefix(9))
+                                .foregroundColor(.white)
+                                .bold()
+                                .font(.system(size: 60))
+                                .padding()
+                        }
+                    }
+                    .padding(.all, 0.0)
+                    .background(Color.gray)
+                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 7)
+                    .cornerRadius(/*@START_MENU_TOKEN@*/6.0/*@END_MENU_TOKEN@*/)
+                    .padding()
+                    Spacer()
+
+                }
+                
+                VStack {
+                    Spacer()
+                    // Our buttons
+                    ForEach(0 ..< 5 ) { i in
+                        HStack {
+                            ForEach(buttons[i], id: \.id) { item in
+                                Button(action: {
+                                    calculator.buttonpress(button: item)
+                                }) {
+                                    Text(item.sign)
+                                        .foregroundColor(Color.black)
+                                        .frame(width: 90, height: 90)
+                                        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.yellow/*@END_MENU_TOKEN@*/)
+                                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/3/*@END_MENU_TOKEN@*/)
+                                        .cornerRadius(/*@START_MENU_TOKEN@*/4.0/*@END_MENU_TOKEN@*/)
+                                        .font(.system(size: 30))
+                                    
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+.previewInterfaceOrientation(.portrait)
     }
 }
